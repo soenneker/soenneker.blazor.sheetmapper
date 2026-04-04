bc4f9f2e3d379d145160bd9f6225381688408cef
Jake Soenneker <jake@soenneker.com>
2026-04-04 08:24:56 -0500

    quark

--- test/Soenneker.Blazor.SheetMapper.Demo/Program.cs
+++ test/Soenneker.Blazor.SheetMapper.Demo/Program.cs
@@ -10,6 +10,7 @@ using Microsoft.JSInterop;
 using Serilog;
 using Serilog.Debugging;
 using Soenneker.Blazor.SheetMapper.Registrars;
+using Soenneker.Quark.Gen.Lucide.Generated;
 using Soenneker.Serilog.Sinks.Browser.Blazor.Registrars;
 
 namespace Soenneker.Blazor.SheetMapper.Demo;
@@ -34,6 +35,7 @@ public sealed class Program
 
             builder.Services.AddSheetMapperAsScoped();
             builder.Services.AddQuarkSuiteAsScoped();
+            builder.Services.AddLucideIconsAsScoped();
 
             WebAssemblyHost host = builder.Build();
 
